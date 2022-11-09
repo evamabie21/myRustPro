87d45f03a3a2fb33895edd0b9cfb7c803ef5b8e1
emabie21 <eva.mabie21@gmail.com>
2022-11-08 23:16:43 -0800

    initial checkin

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut x = 3;
+    println!("The value of x = {x}");
+    x=6;
+    println!("The value of x = {x}");
+
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
+    let x = x * 2;
+
+    println!( "New value is {x}")
+}
